f946dbafdcc3e6bfe55570100c4f74e0d554ecb9
ctucker4 <christinetucker00@gmail.com>
2020-04-20 23:26:03 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,20 +3,28 @@
 
 
 let cards = ["queen", "queen", "king", "king"];
-
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
 
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+}
 
+function flipCard(cardId){
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1])
 	{ alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
-;
+
+console.log("user flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+}
+
+flipCard(3);
+flipCard(2);
